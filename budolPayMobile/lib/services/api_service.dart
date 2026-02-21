4d83e8f66d7dbe04d624610c17628e506226d0e8
Jon Galvez <galvezjon59@gmail.com>
2026-02-21 21:57:45 +0800

    chore:_Increment_mobile_app_version_to_1.3.71

--- budolPayMobile/lib/services/api_service.dart
+++ budolPayMobile/lib/services/api_service.dart
@@ -58,7 +58,7 @@ class ApiService extends ChangeNotifier {
   Map<String, dynamic>? _systemSettings;
   String? _deviceId;
   bool _hasSeenAds = false;
-  String _appVersion = '1.3.69'; // v1.3.69 - Stable Release
+  String _appVersion = '1.3.71'; // v1.3.71 - Taglish Update
 
   String get appVersion => _appVersion;
   Future<void>? _initFuture;
