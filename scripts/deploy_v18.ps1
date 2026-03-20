0dffc891905de22e399ca9fd449d12ddcae35d0f
Jon Galvez <galvezjon59@gmail.com>
2026-03-20 12:03:23 +0800

    Fix ECR repository names in deployment script

--- scripts/deploy_v18.ps1
+++ scripts/deploy_v18.ps1
@@ -10,35 +10,35 @@ aws ecr get-login-password --region $AWS_REGION | docker login --username AWS --
 
 # 1. BudolShap
 echo "Building BudolShap..."
-docker build -t "budolecosystem/budolshap:$IMAGE_TAG" ./budolshap-0.1.0
-docker tag "budolecosystem/budolshap:$IMAGE_TAG" "$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/budolecosystem/budolshap:$IMAGE_TAG"
-docker push "$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/budolecosystem/budolshap:$IMAGE_TAG"
+docker build -t "budol-shap:$IMAGE_TAG" ./budolshap-0.1.0
+docker tag "budol-shap:$IMAGE_TAG" "$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/budol-shap:$IMAGE_TAG"
+docker push "$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/budol-shap:$IMAGE_TAG"
 
 # 2. BudolID
 echo "Building BudolID..."
-docker build -t "budolecosystem/budolid:$IMAGE_TAG" ./budolID-0.1.0
-docker tag "budolecosystem/budolid:$IMAGE_TAG" "$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/budolecosystem/budolid:$IMAGE_TAG"
-docker push "$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/budolecosystem/budolid:$IMAGE_TAG"
+docker build -t "budol-id:$IMAGE_TAG" ./budolID-0.1.0
+docker tag "budol-id:$IMAGE_TAG" "$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/budol-id:$IMAGE_TAG"
+docker push "$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/budol-id:$IMAGE_TAG"
 
 # 3. BudolAccounting
 echo "Building BudolAccounting..."
-docker build -t "budolecosystem/budolaccounting:$IMAGE_TAG" -f ./budolAccounting-0.1.0/Dockerfile .
-docker tag "budolecosystem/budolaccounting:$IMAGE_TAG" "$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/budolecosystem/budolaccounting:$IMAGE_TAG"
-docker push "$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/budolecosystem/budolaccounting:$IMAGE_TAG"
+docker build -t "budol-accounting:$IMAGE_TAG" -f ./budolAccounting-0.1.0/Dockerfile .
+docker tag "budol-accounting:$IMAGE_TAG" "$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/budol-accounting:$IMAGE_TAG"
+docker push "$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/budol-accounting:$IMAGE_TAG"
 
 # 4. BudolWS
 echo "Building BudolWS..."
-docker build -t "budolecosystem/budolws:$IMAGE_TAG" ./websocket-server
-docker tag "budolecosystem/budolws:$IMAGE_TAG" "$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/budolecosystem/budolws:$IMAGE_TAG"
-docker push "$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/budolecosystem/budolws:$IMAGE_TAG"
+docker build -t "budol-ws:$IMAGE_TAG" ./websocket-server
+docker tag "budol-ws:$IMAGE_TAG" "$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/budol-ws:$IMAGE_TAG"
+docker push "$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/budol-ws:$IMAGE_TAG"
 
 # 5. BudolPay (Multi-service)
 $services = @("gateway", "auth", "wallet", "transaction")
 foreach ($service in $services) {
     echo "Building BudolPay $service..."
-    docker build --target $service -t "budolecosystem/budolpay-${service}:$IMAGE_TAG" ./budolpay-0.1.0
-    docker tag "budolecosystem/budolpay-${service}:$IMAGE_TAG" "$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/budolecosystem/budolpay-${service}:$IMAGE_TAG"
-    docker push "$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/budolecosystem/budolpay-${service}:$IMAGE_TAG"
+    docker build --target $service -t "budolpay-${service}:$IMAGE_TAG" ./budolpay-0.1.0
+    docker tag "budolpay-${service}:$IMAGE_TAG" "$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/budolpay-${service}:$IMAGE_TAG"
+    docker push "$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/budolpay-${service}:$IMAGE_TAG"
 }
 
 echo "All images pushed successfully!"
