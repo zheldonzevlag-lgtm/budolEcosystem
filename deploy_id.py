dce37c0ad331c731f393c33ddaad223ccead2ff1
Jon Galvez <galvezjon59@gmail.com>
2026-03-29 07:44:04 +0800

    v28: Finalized Vercel + Render hybrid configuration and documentation

--- deploy_id.py
+++ deploy_id.py
@@ -6,11 +6,11 @@ try:
     subprocess.check_call("aws ecr get-login-password --region ap-southeast-1 | docker login --username AWS --password-stdin 194442925745.dkr.ecr.ap-southeast-1.amazonaws.com", shell=True)
 
     print("Building budol-id Docker...")
-    subprocess.check_call("docker build -t budol-id:v18 ./budolid-0.1.0", shell=True)
+    subprocess.check_call("docker build -t budol-id:v28 ./budolid-0.1.0", shell=True)
 
     print("Pushing to ECR...")
-    subprocess.check_call("docker tag budol-id:v18 194442925745.dkr.ecr.ap-southeast-1.amazonaws.com/budol-id:v18", shell=True)
-    subprocess.check_call("docker push 194442925745.dkr.ecr.ap-southeast-1.amazonaws.com/budol-id:v18", shell=True)
+    subprocess.check_call("docker tag budol-id:v28 194442925745.dkr.ecr.ap-southeast-1.amazonaws.com/budol-id:v28", shell=True)
+    subprocess.check_call("docker push 194442925745.dkr.ecr.ap-southeast-1.amazonaws.com/budol-id:v28", shell=True)
 
     print("Forcing ECS Deployment Reboot...")
     subprocess.check_call("aws ecs update-service --cluster budol-ecosystem-cluster --service budol-id-service --force-new-deployment --region ap-southeast-1 --no-cli-pager", shell=True)
--- deploy_shap.py
+++ deploy_shap.py
@@ -3,11 +3,11 @@ import sys
 
 try:
     print("Building budolshap Docker...")
-    subprocess.check_call("docker build -t budolshap:v18 ./budolshap-0.1.0", shell=True)
+    subprocess.check_call("docker build -t budolshap:v28 ./budolshap-0.1.0", shell=True)
 
     print("Pushing to ECR...")
-    subprocess.check_call("docker tag budolshap:v18 194442925745.dkr.ecr.ap-southeast-1.amazonaws.com/budol-shap:v18", shell=True)
-    subprocess.check_call("docker push 194442925745.dkr.ecr.ap-southeast-1.amazonaws.com/budol-shap:v18", shell=True)
+    subprocess.check_call("docker tag budolshap:v28 194442925745.dkr.ecr.ap-southeast-1.amazonaws.com/budol-shap:v28", shell=True)
+    subprocess.check_call("docker push 194442925745.dkr.ecr.ap-southeast-1.amazonaws.com/budol-shap:v28", shell=True)
 
     print("Forcing ECS Deployment Reboot...")
     subprocess.check_call("aws ecs update-service --cluster budol-ecosystem-cluster --service budol-shap-service --force-new-deployment --region ap-southeast-1 --no-cli-pager", shell=True)
